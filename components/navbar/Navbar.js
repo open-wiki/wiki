4333e519d1554c70b08f5012b0b01c97f4ae57ce
DylanThijs <dylan.thijs@student.hu.nl>
2021-03-29 14:38:27 +0200

    Active links & hamburger

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,23 +1,37 @@
-import Link from 'next/link'
 import Styles from './Navbar.module.css'
+import React, { useState } from 'react'
+import ActiveLink from '../activelink/Activelink'
 
-const Navbar = () => {
+export default function Navbar() {
+  const [open, setOpen] = useState(false)
+  function toggle() {
+    setOpen(!open)
+  }
+
   return (
-    <nav className={Styles.navbar}>
-      <div className={'logo'}>
-        <h2>Open Wiki</h2>
+    <>
+      <div className={Styles.hamburgerOpen} onClick={toggle}>
+        <i className="material-icons">menu</i>
       </div>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      <Link href="/articles">
-        <a>Overview</a>
-      </Link>
-      <Link href="/articles/create">
-        <a>Create</a>
-      </Link>
-    </nav>
+      <nav className={open ? Styles.navbar : Styles.closednavbar}>
+        <div className={Styles.logo}>
+          <h2>Open Wiki</h2>
+        </div>
+        <div className={Styles.hamburgerClosed} onClick={toggle}>
+          <i className="material-icons">close</i>
+        </div>
+        <div className={Styles.menu}>
+          <ActiveLink activeClassName={Styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={Styles.active} href="/articles">
+            <a>Overview</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={Styles.active} href="/articles/create">
+            <a>Create</a>
+          </ActiveLink>
+        </div>
+      </nav>
+    </>
   )
 }
-
-export default Navbar
