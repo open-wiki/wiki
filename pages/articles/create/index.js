86f363bf04485055f4c8c9a33816e07c58f6108b
thijspoon28 <thijspoon28@gmail.com>
2021-03-29 14:07:39 +0200

    create pagina database ding

--- pages/articles/create/index.js
+++ pages/articles/create/index.js
@@ -1,14 +1,32 @@
 import NormEdit from '../../../components/markdown-editor/MarkdownEditor'
-import Link from 'next/link'
-import Styles from './create.module.css'
+import Create_Article from '../../api/create'
+import * as React from 'react'
+import Styles from '../../../build/server/pages/articles/create'
 
 export default function Index() {
+  const [value, setValue] = React.useState('**Hello world!!!**')
+  const [title, setTitle] = React.useState('title')
+  const sendDataToParent = (index) => {
+    // the callback. Use a better name
+    console.log(index)
+    setValue(index)
+  }
+  const handleSubmit = (event) => {
+    Create_Article(title, value)
+    event.preventDefault()
+    console.log(title, value)
+  }
   return (
     <div>
-      <NormEdit></NormEdit>
-      <Link href="/articles">
-        <button className={Styles.button}>Submit</button>
-      </Link>
+      <form onSubmit={(event) => handleSubmit(event)}>
+        <input
+          type="text"
+          name="title"
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <NormEdit sendDataToParent={sendDataToParent} value={value} />
+        <input type="submit" value="Submit" className={Styles.button} />
+      </form>
     </div>
   )
 }
