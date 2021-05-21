d2ac60ecb67a6044f1b633f5523ede9a6a26742c
mitchellvdhut <mitchellvdhut@gmail.com>
2021-05-21 08:46:38 +0200

    added forms

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,26 +1,57 @@
 import React, { useState } from 'react'
 import LoginFormToggle from '../../components/login-form-toggle'
+import Styles from './login.module.css'
 
 const Login = () => {
   const [showRegister, setShowRegister] = useState(false)
 
   return (
     <>
-      <h2>CSS Tab</h2>
+      <h2>Login/Register</h2>
       <div>
         <LoginFormToggle
-          label="tab 1"
+          label="login"
           setShowRegister={() => setShowRegister(false)}
           active={!showRegister}
         />
         <LoginFormToggle
-          label="tab 2"
+          label="register"
           setShowRegister={() => setShowRegister(true)}
           active={showRegister}
         />
-        {showRegister ? <div>content 2</div> : <div>content 1</div>}
+        {showRegister ? <RegisterForm /> : <LoginForm />}
       </div>
     </>
+  )
+}
+const LoginForm = () => {
+  const [loginEmail, setLoginEmail] = useState()
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(loginEmail)
+  }
+  return (
+    <form className={Styles.loginForm} onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="voornaam.achternaam@student.hu.nl"
+        value={loginEmail}
+        onChange={(e) => setLoginEmail(e.target.value)}
+      />
+      <input type="password" placeholder="wachtwoord" />
+      <input type="submit" />
+    </form>
+  )
+}
+const RegisterForm = () => {
+  return (
+    <form className={Styles.registerForm}>
+      <input type="text" placeholder="voornaam" />
+      <input type="text" placeholder="achternaam" />
+      <input type="email" placeholder="email" />
+      <input type="password" placeholder="wachtwoord" />
+      <input type="password" placeholder="herhaal wachtwoord" />
+    </form>
   )
 }
 
